5935a449525dceb9cbc4d7b30014c648b2df0d96
wrdg <officialwardog@gmail.com>
2021-02-21 22:27:43 -0600

    Removed mission redundancy for OnScriptLog

--- WRDG/BashClient/Scripts/5_Mission/BashClient/Mission/MissionBase.c
+++ WRDG/BashClient/Scripts/5_Mission/BashClient/Mission/MissionBase.c
@@ -8,4 +8,18 @@ modded class MissionBase
 			//GetGame().RequestExit(0);
 		}
 	}
+
+	override void OnEvent(EventType eventTypeId, Param params)
+	{
+		super.OnEvent(eventTypeId, params);
+
+		switch (eventTypeId)
+		{
+			case ScriptLogEventTypeID:
+			{
+				Param1<string> log_params = Param1<string>.Cast(params);
+				GetModuleManager().OnScriptLog(log_params.param1);
+			}
+		}
+	}
 }
