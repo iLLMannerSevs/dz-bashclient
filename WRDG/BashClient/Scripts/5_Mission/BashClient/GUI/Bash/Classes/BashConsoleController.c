c0d1ce65144844d4009412021696a44bf0d87df7
wrdg <officialwardog@gmail.com>
2021-02-21 23:26:59 -0600

    Basic execution functionality, client only

--- WRDG/BashClient/Scripts/5_Mission/BashClient/GUI/Bash/Classes/BashConsoleController.c
+++ WRDG/BashClient/Scripts/5_Mission/BashClient/GUI/Bash/Classes/BashConsoleController.c
@@ -1,4 +1,6 @@
 class BashConsoleController : Controller
 {
+	string ConsoleInput;
+
 	ref ObservableCollection<ref BashTextEntryView> LogEntries = new ObservableCollection<ref BashTextEntryView>(this);
 }
