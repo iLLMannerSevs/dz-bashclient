a7c91330317879b23d6838a71e7ba923b079e45d
wrdg <officialwardog@gmail.com>
2021-02-21 23:09:53 -0600

    New console layout

--- WRDG/BashClient/Scripts/5_Mission/BashClient/GUI/Bash/BashConsoleView.c
+++ WRDG/BashClient/Scripts/5_Mission/BashClient/GUI/Bash/BashConsoleView.c
@@ -1,23 +1,23 @@
 class BashConsoleView : ScriptViewTemplate<BashConsoleController>
 {
 	static const int MAX_LINE_COUNT = 100;
-	
+
 	void AddReadoutLine(string message)
 	{
-		if (GetTemplateController().LogEntries) 
+		if (GetTemplateController().LogEntries)
 		{
 			GetTemplateController().LogEntries.Insert(new BashTextEntryView(message));
-			
+
 			// Remove line when exceeds max lines
-			if (GetTemplateController().LogEntries.Count() >= MAX_LINE_COUNT) 
+			if (GetTemplateController().LogEntries.Count() >= MAX_LINE_COUNT)
 			{
 				GetTemplateController().LogEntries.Remove(0);
 			}
 		}
 	}
-	
+
 	override string GetLayoutFile()
 	{
-		return "WRDG/BashClient/GUI/Layouts/Console/console_window.layout";
+		return "WRDG/BashClient/GUI/Layouts/Console/console_new.layout";
 	}
 }
