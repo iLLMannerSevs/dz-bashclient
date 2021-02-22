cb679c27cc8d1e56f0357be949e84de161799a17
wrdg <officialwardog@gmail.com>
2021-02-21 22:42:10 -0600

    Revert "Removed mission redundancy for OnScriptLog"
    This reverts commit 5935a449525dceb9cbc4d7b30014c648b2df0d96.

--- WRDG/BashClient/Scripts/5_Mission/BashClient/Mission/MissionGameplay.c
+++ WRDG/BashClient/Scripts/5_Mission/BashClient/Mission/MissionGameplay.c
@@ -1,4 +1,16 @@
 modded class MissionGameplay
 {
+	override void OnEvent(EventType eventTypeId, Param params)
+	{
+		super.OnEvent(eventTypeId, params);
 
+		switch (eventTypeId)
+		{
+			case ScriptLogEventTypeID:
+			{
+				Param1<string> log_params = Param1<string>.Cast(params);
+				GetModuleManager().OnScriptLog(log_params.param1);
+			}
+		}
+	}
 }
