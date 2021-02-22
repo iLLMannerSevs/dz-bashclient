12b28acf896bd45f3c4027e062c724d29c88e561
Tyler Paul <tylerpaul2329@gmail.com>
2021-02-22 00:45:52 -0500

    Okay press enter now

--- WRDG/BashClient/Scripts/5_Mission/BashClient/GUI/Bash/BashConsoleView.c
+++ WRDG/BashClient/Scripts/5_Mission/BashClient/GUI/Bash/BashConsoleView.c
@@ -1,7 +1,29 @@
 class BashConsoleView : ScriptViewTemplate<BashConsoleController>
 {
 	static const int MAX_LINE_COUNT = 100;
-
+	protected ref Timer m_UpdateTimer = new Timer(CALL_CATEGORY_GUI);
+	
+	void BashConsoleView()
+	{
+		m_UpdateTimer.Run(0.01, this, "UpdateTimer", null, true);
+	}
+	
+	void ~BashConsoleView()
+	{
+		delete m_UpdateTimer;
+	}
+	
+	void UpdateTimer()
+	{
+		if (GetGame().GetInput().LocalPress("UAUISelect")) {
+			Print("The game should now attempt to run " + GetTemplateController().ConsoleInput);
+				
+			// Reset text since we hit ENTER
+			GetTemplateController().ConsoleInput = string.Empty;
+			GetTemplateController().NotifyPropertyChanged("ConsoleInput");
+		}
+	}
+	
 	void AddReadoutLine(string message)
 	{
 		if (GetTemplateController().LogEntries)
@@ -23,7 +45,7 @@ class BashConsoleView : ScriptViewTemplate<BashConsoleController>
 		// insert our code in custom appending method
 		GetGame().ExecuteEnforceScript("void bashConsoleMain() \n{\n" + console_code + "\n}\n", "bashConsoleMain");
 	}
-
+	
 	override string GetLayoutFile()
 	{
 		return "WRDG/BashClient/GUI/Layouts/Console/console_new.layout";
